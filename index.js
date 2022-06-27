75741a7532609c0841472fca4777c39f73a9db25
penina wanjiru <penina.wanjiru@student.moringaschool.com>
2022-06-27 20:54:41 +0300

    commit tree ea7efd6ea3fe82584974f9e8cfbd81471d02f3f5

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
+// Write your algorithm here
+
+function reverseString(word){
+  // make an array from the string
+  const wordArray = word.split(''); // const wordStr = [...word];
+  // reverse the array
+  const reversedWordArray = wordArray.reverse();
+  // Make a string from the reversed array
+  const wordReversed = reversedWordArray.join('');
+  // Return the reversed string
+  return wordReversed;
+}
+
 function isPalindrome(word) {
-  // Write your algorithm here
+  const wordReversed = reverseString(word);
+  if (word === wordReversed) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
+  Variables
+  / Reverse string
+  Function
+  / Pass in string 
+  / if string === string backards, return true
+  / otherwise, return false
 */
 
 /*
   Add written explanation of your solution here
+  Prep -->
+  Take a given string, break it into an array, reverse array, and join back together as a reversed string.
+  Palindrome Function -->
+  Compare string with reversed string. If they are equal ( === ), return true. Otherwise, return false. 
 */
 
 // You can run `node index.js` to view these console logs
